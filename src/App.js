d58a4ceba76c732506f978cbdc1ecdbf8f89f0b1
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-03-01 21:35:27 +0530

    Navbar complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Grievance from "./components/Grievance";
+import Playground from "./components/Playground";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/grievance" component={Grievance} />
+            <Route exact path="/playground" component={Playground} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Grievance.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Grievance extends Component {
+  render() {
+    return <div>Grievance</div>;
+  }
+}
