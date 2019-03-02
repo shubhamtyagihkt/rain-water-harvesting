9cdbd5ca2e89234e31af6816cb9c7ec59f10be5b
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-03-02 10:05:02 +0530

    Added google map

--- src/components/OptimalMap.js
+++ src/components/OptimalMap.js
@@ -1,7 +1,42 @@
+/*global google*/
 import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      )}
+    </GoogleMap>
+  ))
+);
 
 export default class OptimalMap extends Component {
   render() {
-    return <div>Hi</div>;
+    return (
+      <MyMapComponent
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={
+          <div
+            style={{
+              height: `980px`,
+              marginLeft: 0,
+              paddingLeft: 0,
+              marginRight: "10px"
+            }}
+          />
+        }
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
   }
 }
