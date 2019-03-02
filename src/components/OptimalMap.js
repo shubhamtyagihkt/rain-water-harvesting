7caecfb3f95f68a050c1d9edf76a22b502dfc9f6
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-03-02 09:48:05 +0530

    Before maps

--- src/components/OptimalMap.js
+++ src/components/OptimalMap.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class OptimalMap extends Component {
   render() {
-    return <div id="map" style={{ height: "400px", width: "100%" }} />;
+    return <div>Hi</div>;
   }
 }
